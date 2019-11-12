223da8c62a99ea62b880acb94b948bd79cdb343b
Phani <phani.mfe@gmail.com>
2019-11-11 20:16:10 -0500

    with timer

--- src/ps_timeit.py
+++ src/ps_timeit.py
@@ -1,6 +1,15 @@
+def _redirect_out(message,**kwargs):
+    if kwargs and 'app_name_log' in kwargs:
+        import logging
+        log_name = kwargs.get('app_name_log')
+        logger = logging.getLogger(log_name)
+        logger.info(message)
+    else:
+        print(message)
+
 def ps_timeit(function):
     '''
-    Phani Sarma : decorator for timing a particular function execution
+    Phani Sarma timeit : decorator for timing a particular function execution
     everytime a function is executed, its execution is measured in secs
     if app_name_log is specified, timing is redirected to the respective app logger so there is continuity
     else: standard print redirection of message
@@ -14,15 +23,45 @@ def ps_timeit(function):
         result = function(*args,**kwargs)
         end    = time.time()
 
-        duration = int((end - start) * 1000)
+        duration = int((end - start))
         funcname = function.__name__
         msg      = '%s took %d secs' % (funcname, duration)
-        if 'app_name_log' in kwargs:
-            import logging
-            log_name = kwargs.get('app_name_log')
-            logger = logging.getLogger(log_name)
-            logger.info(msg)
-        else:
-            print(msg)
+        _redirect_out(msg,**kwargs)
         return result
     return timer
+
+
+class PSC_Timer():
+    def __init__(self,app_name_log=None):
+        self.app_name_log = app_name_log
+
+    def __enter__(self):
+        import time
+        from inspect import currentframe,getouterframes
+
+        calframe  =  getouterframes(currentframe(), 2)
+        self.msg = 'function: %s line_no_begin: %d '%(calframe[1][3],calframe[1][2])
+        self.start_time = time.clock()
+
+    def __exit__(self, *args):
+        import time
+        from inspect import currentframe, getframeinfo,getouterframes
+
+        self.end_time = time.clock()
+        self.duration = self.end_time - self.start_time
+        calframe  =  getouterframes(currentframe(), 2)
+        self.msg += ' line_no_end: %d took %d secs '%(calframe[1][2],self.duration)
+
+        kwargs = {'app_name_log':self.app_name_log } if self.app_name_log else {}
+        _redirect_out(self.msg,**kwargs)
+
+
+
+
+
+
+
+
+
+
+
--- tests/test_timeit.py
+++ tests/test_timeit.py
@@ -1,10 +1,8 @@
 from nose import with_setup
-from nose.tools import nottest,raises,assert_equal,assert_equals,assert_almost_equal
+from nose.tools import nottest
 
 
-from src.ps_timeit import ps_timeit
-
-thrdpool = None
+from src.ps_timeit import ps_timeit,PSC_Timer
 
 @nottest
 def setup():
@@ -12,11 +10,27 @@ def setup():
 
 
 @with_setup(setup)
-def test_mapfunction():
-
+def test_timerdecor():
+    import time
     @ps_timeit
     def calc_square(x):
+        time.sleep(5)# just so we elapse
         return x**2
 
     calc_square(40000)
-    #assert True
+
+    assert True
+
+@with_setup(setup)
+def test_timerscope():
+    import time
+    def calc_square(x):
+        return x**2
+
+    with PSC_Timer() as t:
+         calc_square(40000)
+         time.sleep(5)# just so we elapse
+         calc_square(1000000000)
+
+    assert True
+
